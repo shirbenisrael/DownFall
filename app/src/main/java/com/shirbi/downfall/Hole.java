c4958911a31ea4574bbe415f745da48f6d362b0e
Shir Ben-Israel <shirbenisrael@gmail.com>
2018-12-09 01:34:03 +0200

    Rotate holes without moving them.
    Set hole image to be same size as wheel image.

--- app/src/main/java/com/shirbi/downfall/Hole.java
+++ app/src/main/java/com/shirbi/downfall/Hole.java
@@ -2,7 +2,6 @@ package com.shirbi.downfall;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 public class Hole extends RotatableImage {
@@ -21,25 +20,11 @@ public class Hole extends RotatableImage {
     }
 
     public void SetAngle(double wheelAngle) {
-        RelativeLayout relativeLayout = (RelativeLayout)this.getParent();
-        RelativeLayout.LayoutParams params =
-                new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT);
-
-        double centerXY = relativeLayout.getWidth()/2;
-        double radius = centerXY * 0.8;
+        RelativeLayout relativeLayout = (RelativeLayout) this.getParent();
         double angle = wheelAngle + m_baseAngle;
-        double angleRadians = Math.toRadians(angle);
-        double leftFromCenter = radius * Math.sin(angleRadians);
-        double topFromCenter = -radius * Math.cos(angleRadians);
 
-        params.leftMargin = (int)(centerXY + leftFromCenter);
-        params.topMargin  = (int)(centerXY + topFromCenter);
-        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-        params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
-
-        relativeLayout.removeView(this);
-        relativeLayout.addView(this, params);
+        getLayoutParams().width = relativeLayout.getLayoutParams().width;
+        getLayoutParams().height = relativeLayout.getLayoutParams().height;
 
         super.Rotate(angle);
     }
