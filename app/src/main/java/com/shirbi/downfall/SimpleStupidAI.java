4f57261cc2bc2259192d2f7a79d7367dffccaba2
Shir Ben-Israel <shirbenisrael@gmail.com>
2018-12-22 15:01:40 +0200

    Simple Stupid AI: Don't hurt current token/hole position
    Before this fix, AI may rotate wheel to put one token/hole in good
    position while moving other toke/hole which was already in good
    position.

--- app/src/main/java/com/shirbi/downfall/SimpleStupidAI.java
+++ app/src/main/java/com/shirbi/downfall/SimpleStupidAI.java
@@ -87,7 +87,7 @@ public class SimpleStupidAI {
         return null;
     }
 
-    private RotationResult RotateWheelResultTokenBetterPosition(Wheel wheel) {
+    private Boolean WheelHasTokenInGoodPosition(Wheel wheel) {
         for (Hole hole : wheel.m_holes) {
             if (!hole.GetOppositeSide()) {
                 // Simple stupid AI doesn't care about the human player tokens
@@ -97,15 +97,31 @@ public class SimpleStupidAI {
                 continue;
             }
 
+            // If we got here, we have a hole with our token.
+            // Lets see if it is connected as top.
             for (Connection connection : wheel.m_connections) {
                 if (connection.m_top_wheel != wheel) {
                     continue;
                 }
 
                 if (connection.GetTopConnected(hole) == hole) {
-                    // Hole is already connected
-                    return null;
+                    // Hole is already connected as top.
+                    return true;
                 }
+            }
+        }
+
+        return false;
+    }
+
+    private RotationResult RotateWheelResultTokenBetterPosition(Wheel wheel) {
+        for (Hole hole : wheel.m_holes) {
+            if (!hole.GetOppositeSide()) {
+                // Simple stupid AI doesn't care about the human player tokens
+                continue;
+            }
+            if (hole.GetResident() == null) {
+                continue;
             }
 
             for (Connection connection : wheel.m_connections) {
@@ -130,18 +146,11 @@ public class SimpleStupidAI {
         return null;
     }
 
-    private RotationResult RotateWheelResultHoleBetterPosition(Wheel wheel) {
-
+    private Boolean WheelHasHoleInGoodPosition(Wheel wheel) {
         for (Hole hole : wheel.m_holes) {
             if (hole.GetOppositeSide()) {
                 // Simple stupid AI doesn't care about the human player tokens
                 continue;
-            }
-
-            // If wheel has tokens and we reach this function, it means they are already in good place.
-            // Better not move this wheel.
-            if (hole.GetResident() != null) {
-                return null;
             }
 
             for (Connection connection : wheel.m_connections) {
@@ -149,13 +158,17 @@ public class SimpleStupidAI {
                     continue;
                 }
 
-                if (connection.GetTopConnected(hole) == hole) {
+                if (connection.GetBottomConnected(hole) == hole) {
                     // Empty hole is already connected to other top wheel.
-                    return null;
+                    return true;
                 }
             }
         }
 
+        return false;
+    }
+
+    private RotationResult RotateWheelResultHoleBetterPosition(Wheel wheel) {
         for (Hole hole : wheel.m_holes) {
             if (!hole.GetOppositeSide()) {
                 // Simple stupid AI doesn't care about the human player tokens
@@ -195,10 +208,18 @@ public class SimpleStupidAI {
             return result_will_fall_token;
         }
 
+        if (WheelHasTokenInGoodPosition(wheel)) {
+            return null;
+        }
+
         // Can't fall any token? at least put a token in better position so it can fall later.
         RotationResult result_will_move_token = RotateWheelResultTokenBetterPosition(wheel);
         if (result_will_move_token != null) {
             return result_will_move_token;
+        }
+
+        if (WheelHasHoleInGoodPosition(wheel)) {
+            return null;
         }
 
         // Can't put token in better position? At least put a hole in better position.
