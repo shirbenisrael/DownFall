db6abd51951dde0cd9fc2f8d59ba8a67feeaba9c
Shir Ben-Israel <shirbenisrael@gmail.com>
2018-12-29 19:13:54 +0200

    Store number of tokens left for each player.

--- app/src/main/java/com/shirbi/downfall/Output.java
+++ app/src/main/java/com/shirbi/downfall/Output.java
@@ -1,6 +1,7 @@
 package com.shirbi.downfall;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 
@@ -39,6 +40,28 @@ public class Output extends ConnectableImage implements SlideToken {
         for (int i = 0; i < m_player_num_tokens_left.length; i++) {
             m_player_num_tokens_left[i] = 10;
             m_activity.ShowNumTokenLeft(PlayerType.values()[i], m_player_num_tokens_left[i]);
+        }
+    }
+
+    public void StoreState(SharedPreferences.Editor editor) {
+        for (Hole hole : m_holes) {
+            int player_num = hole.GetPlayerType().getInt();
+
+            String str = (m_activity.getString(R.string.output_queue_tokens)) + player_num;
+
+            editor.putInt(str, m_player_num_tokens_left[player_num]);
+        }
+    }
+
+    public void RestoreState(SharedPreferences sharedPref) {
+        for (Hole hole : m_holes) {
+            int player_num = hole.GetPlayerType().getInt();
+
+            String str = (m_activity.getString(R.string.output_queue_tokens)) + player_num;
+
+            m_player_num_tokens_left[player_num] = sharedPref.getInt(str, 10);
+
+            m_activity.ShowNumTokenLeft(hole.GetPlayerType(),  m_player_num_tokens_left[player_num]);
         }
     }
 }
